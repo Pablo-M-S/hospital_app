2e6c4af37c010dc5413dd9d0d2abcb014a017e79
Pablo-M-S <munizp900@gmail.com>
2026-09-07 18:23:47 -0300

    Delete src/routes/authRoutes.js

--- src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { login, registrar } = require('../controllers/authController');
-
-router.post('/login', login);
-router.post('/registrar', registrar);
-
-module.exports = router;
