a381eda1e56db8026a3516e6ae0b06805ee13ef0
Pablo-M-S <munizp900@gmail.com>
2026-09-07 19:23:27 -0300

    Update asyncHandler.ts

--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction, RequestHandler } from 'express';
 type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<unknown>;
 
 export function asyncHandler(fn: AsyncHandler): RequestHandler {
-  return (req, res, next) => {
+  return (req: Request, res: Response, next: NextFunction) => {
     fn(req, res, next).catch(next);
   };
 }
